449516c8800ff2b59e05bea9d281524b478f3c9d
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-13 21:16:11 +0100

    installs node-sass-middleware, adds sass

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
 const methodOverride = require('method-override');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: __dirname,
+    dest: path.join(__dirname, 'public'),
+    debug: true, 
+    outputStyle: 'compressed',
+    force: [true]
+}));
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('index')
