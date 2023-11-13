feb1c57404f45b2d672e29b0c1b76b9ee9956ac9
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-13 15:21:51 +0100

    starts work on index page

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/listings', (req, res) => {
+    res.render('listings')
+})
 
 
 
