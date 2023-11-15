ecc5da01894ff585254cc35d0dc93dd725ff35ce
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-15 12:09:05 +0100

    adds initial register page

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
 app.get('/listings', (req, res) => {
     res.render('listings')
 })
