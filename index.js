d9649cf64af71c5dc4a501eedc93c81b4d864775
EkaterinaNattrass <cattiva2012@gmail.com>
2023-11-22 11:40:37 +0100

    adds login and register functions

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const sassMiddleware = require('node-sass-middleware');
 
+const loginUser = require('./modules/login');
+const registerUser = require('./modules/register');
+
+const fetch = (...args) => import ('node-fetch').then(({default : fetch}) => fetch(...args));
+
 app.use(sassMiddleware({
     src: __dirname,
     dest: path.join(__dirname, 'public'),
@@ -20,13 +25,71 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'bid-beautifully',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 3600000,
+    }
+}));
+
+app.use(flash());
+
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        failedLoginMessage: req.flash('LoginError')
+    })
+});
+
+app.get('/login-form', async (req, res) => {
+    res.redirect('listings');
 })
 
+app.post('/login-form', async(req, res) => {
+
+    const loggedInUser = await loginUser(req.body);
+
+    if (loggedInUser.success) {
+        req.session.token = loggedInUser.token;
+        req.session.userName = loggedInUser.userName;
+        req.session.credits = loggedInUser.credits;
+        req.session.avatar = loggedInUser.avatar;
+        console.log('went through')
+        res.render('listings');
+    } else {
+        // req.flash ('loginError', loggedInUser.error + '...Something went wrong. Please, try again');
+        res.redirect('/');
+    }
+});
+
 app.get('/register', (req, res) => {
-    res.render('register')
+    res.render('register', {
+        failedRegisterMessage: req.flash('registerError')
+    })
+});
+
+app.get('/register-form', async(req, res) => {
+    res.redirect('/register')
+});
+
+app.post('/register-form', async(req, res) => {
+
+    const registeredUser = await registerUser(req.body);
+
+    if(!registeredUser.success) {
+        req.flash('registerError', registeredUser.error + '...Something went wrong. Please, try again');
+        res.redirect('/listings');
+        return;
+    }
 })
+
+
 
 app.get('/listings', (req, res) => {
     res.render('listings')
@@ -38,6 +101,10 @@ app.get('/profile', (req, res) => {
 
 app.get('/details', (req, res) => {
     res.render('details')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
 })
 
 
---
+++ modules/login.js
@@ -0,0 +1,43 @@
+const loginUser = async function (userData) {
+
+    const url = 'https://api.noroff.dev/api/v1/auction/auth/login';
+
+    returnObj = {
+        'success': false,
+        'userName': "",
+        'token': "",
+        'credits': "",
+        'avatar': "",
+        'error': null
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    };
+
+    try{
+        const res = await fetch(url, options);
+        const data = await res.json();
+        statusCode = res.status;
+
+        if (statusCode === 200) {
+            returnObj.success = true;
+            returnObj.userName = data.name;
+            returnObj.token = data.accessToken;
+        } else {
+            returnObj.error = data.errors[0].message;
+            console.log('error' + JSON.stringify(data));
+        }
+    }
+    catch (err) {
+        console.log('The error is ' + err + err.stattus);
+        returnObj.error = JSON.stringify(err);
+    }
+    return returnObj;
+};
+
+module.exports = loginUser;
